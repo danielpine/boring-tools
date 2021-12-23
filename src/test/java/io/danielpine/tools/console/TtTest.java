19a98ce1e99ad705858df4b027822be351ec18cc
danielpine <danielpine@sina.com>
2021-12-23 17:11:29 +0800

    Tsao's TT Tests.

--- src/test/java/io/danielpine/tools/console/TtTest.java
+++ src/test/java/io/danielpine/tools/console/TtTest.java
@@ -1,14 +1,11 @@
 package io.danielpine.tools.console;
 
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Test;
-import org.springframework.util.StopWatch;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Hyleon
@@ -22,18 +19,37 @@ public class TtTest {
                 new Student("Lisi", 11, 555556666, false, false),
                 new Student("王五", 999, 123, true, false)
         );
+
         int times = 10000;
-        StopWatch stopwatch = new StopWatch();
-        stopwatch.start("TT");
-        for (int i = 0; i < times; i++) {
-            TT.data(students).display();
-        }
-        stopwatch.stop();
-        stopwatch.start("TB");
+
+        long bf = System.currentTimeMillis();
         for (int i = 0; i < times; i++) {
             TableBuilder.newBuilder().display(students);
         }
-        stopwatch.stop();
-        System.out.println(stopwatch.prettyPrint());
+        long af = System.currentTimeMillis() - bf;
+
+        long bf2 = System.currentTimeMillis();
+        for (int i = 0; i < times; i++) {
+            TT.data(students).display();
+        }
+        long af2 = System.currentTimeMillis() - bf2;
+
+        System.out.println("TableBuilder Times:" + times + " spend:" + af);
+        System.out.println("TT           Times:" + times + " spend:" + af2);
+
+        TT<Student> data = TT.data(students);
+
+        // 1. tablefy() returns a tablefied string
+        System.out.println(data.tablefy());
+
+        // 2. directly display (default output stream: System.out)
+        data.display();
+
+        // 3. write to an output stream
+        try {
+            data.display(new FileOutputStream("table.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
